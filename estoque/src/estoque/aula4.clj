4da46dca41127f690ea4c437cdf001b183b46b88
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-07-09 18:03:46 -0300

    Atualiza arquivo da aula 4

--- estoque/src/estoque/aula4.clj
+++ estoque/src/estoque/aula4.clj
@@ -18,3 +18,32 @@
   (+ valor 1))
 
 (println (update precos 0 soma))
+
+(defn aplica-desconto?
+  "Verifica se deve aplicar desconto"
+  [valor-bruto]
+  (> valor-bruto 100))
+
+(defn valor-descontado1
+  "Retorna valor descontado, dependendo do valor bruto"
+  [valor-bruto]
+  (if (aplica-desconto? valor-bruto)
+    (let [taxa-de-desconto (/ 10 100)
+          desconto (* valor-bruto taxa-de-desconto)]
+      (- valor-bruto desconto))
+    valor-bruto))
+
+(println (map valor-descontado1 precos))
+
+(println (range 10))
+(println (filter even? (range 10)))
+
+(println (filter aplica-desconto? precos))
+
+(defn minha-soma
+  [valor-1 valor-2]
+  (+ valor-1 valor-2))
+
+(println (reduce minha-soma 0 precos))
+
+(println (reduce + 0 (range 100)))
