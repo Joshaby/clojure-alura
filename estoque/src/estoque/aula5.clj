9eaf3843fe624334976322e33144a2b1943e6419
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-07-10 00:34:41 -0300

    Adicionado arquivos da aula 5 e 6

---
+++ estoque/src/estoque/aula5.clj
@@ -0,0 +1,54 @@
+(ns estoque.aula5)
+
+(def estoque {"Mochila" 10
+              "Camiseta" 5})
+
+(defn ecamisa?
+  [valor]
+  (= valor "Camiseta"))
+
+(println estoque)
+(println (count estoque))
+(println (filter ecamisa? (keys estoque)))
+(println (keys estoque))
+(println (vals estoque))
+
+(def estoque1 {:Mochila 10
+               :Camiseta 5})
+
+(println (keys estoque1))
+
+(println (assoc estoque1 :Cadeira 3))
+(println (keys estoque1))
+
+(println (update estoque1 :Mochila inc))
+
+(println (dissoc estoque1 :Mochila))
+
+
+(def pedido {:mochila {:quantidade 2 :preco 80}
+             :camiseta {:quantidade 3 :preco 40}})
+
+(println pedido)
+
+(def pedido (assoc pedido :chaveiro {:quantidade 1 :preco 10}))
+
+(println pedido)
+
+(println (get pedido :mochila))
+(println (get pedido :moc {}))
+(println (:mochila pedido))
+(println (:moc pedido {}))
+(println (:quantidade (:mochila pedido)))
+
+(println (update-in pedido [:mochila :quantidade] inc))
+
+; THREADING FIRST
+(println (-> pedido
+             :mochila
+             :quantidade))
+
+(-> pedido
+    :mochila
+    :quantidade
+    println)
---
+++ estoque/src/estoque/aula6.clj
@@ -0,0 +1,92 @@
+(ns estoque.aula6)
+
+(def pedido {:mochila {:quantidade 2 :preco 80}
+             :camiseta {:quantidade 3 :preco 40}})
+
+(defn imprime-e-15
+  [valor]
+  (println valor)
+  (println (class valor))
+  15)
+
+(println (map imprime-e-15 pedido))
+
+(defn imprime
+  [[chave valor]]
+  (println chave "e" valor)
+  (println (class chave) (class valor))
+  valor)
+
+(println (map imprime pedido))
+
+(defn preco-dos-produtos
+  [[_ valor]]
+  (* (-> valor :quantidade) (-> valor :preco)))
+
+(println (reduce + 0 (map preco-dos-produtos pedido)))
+
+(defn total-pedido
+  [pedido]
+  (reduce + 0 (map preco-dos-produtos pedido)))
+
+(println (total-pedido pedido))
+
+; THREADING LAST
+(defn total-pedido1
+  [pedido]
+  (->> pedido
+      (map preco-dos-produtos)
+      (reduce + 0)))
+
+(println (total-pedido1 pedido))
+
+(defn preco-dos-produtos1
+  [produto]
+  (* (-> produto :quantidade) (-> produto :preco)))
+
+(defn total-pedidos2
+  [pedido]
+  (->> pedido
+       vals
+       (map preco-dos-produtos1)
+       (reduce + 0)))
+
+(println (total-pedidos2 pedido))
+
+(def pedido {:mochila {:quantidade 2 :preco 80}
+             :camiseta {:quantidade 3 :preco 40}
+             :chaveiro {:quantidade 1}})
+
+(defn gratuito?
+  [item]
+  (<= (get item :preco 0) 0))
+
+(println (filter gratuito? (vals pedido)))
+
+(defn gratuito1?
+  [[_ item]]
+  (<= (get item :preco 0) 0))
+
+(println (filter gratuito1? pedido))
+(println (filter (fn [[_ item]] (gratuito? item)) pedido))
+(println (filter #(gratuito? (second %)) pedido))
+
+(defn pago?
+  [item]
+  (not (gratuito? item)))
+
+(println (pago? {:preco 50}))
+
+(def pago1? (comp not gratuito?))
+
+(println (pago1? {:preco 50}))
+
+(def clientes [
+               { :nome "Guilherme"
+                :certificados ["Clojure" "Java" "Machine Learning"] }
+               { :nome "Paulo"
+                :certificados ["Java" "Ciência da Computação"] }
+               { :nome "Daniela"
+                :certificados ["Arquitetura" "Gastronomia"] }])
+
+(->> clientes (map :certificados) (map count) (reduce +) println)
