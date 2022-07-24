ec8d62ea7b8c4906e26dcd587e9011bcc6cf9ed9
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-07-24 20:10:51 -0300

    Adiciona .gitignore

--- src/loja_curso2/aula2.clj
+++ src/loja_curso2/aula2.clj
@@ -4,6 +4,20 @@
 
 (defn conta
   [total-ate-agora elementos]
-  (recur (inc total-ate-agora) (rest elementos)))
+  (if (next elementos)
+    (recur (inc total-ate-agora) (next elementos))
+    (+ 1 total-ate-agora)))
 
-(println (conta 0 vetor))
+(println (conta 0 []))
+
+(defn conta1
+  ([total-ate-agora elementos]
+   (if (seq elementos)
+     (recur (inc total-ate-agora) (next elementos))
+     total-ate-agora))
+  ([elementos] (conta1 0 elementos)))
+
+(println (conta1 0 vetor))
+(println (conta1 0 []))
+(println (conta1 vetor))
+(println (conta1 []))
